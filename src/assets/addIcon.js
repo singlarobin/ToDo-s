525b72c249aa300bfb6bb83accdc8104d2ff3e42
singlarobin <singlarobin99@gmail.com>
2021-07-28 23:04:55 +0530

    indentation correction is done.

--- src/assets/addIcon.js
+++ src/assets/addIcon.js
@@ -1,12 +1,12 @@
 const AddIcon = props => {
     const { width, height, fill, strokeColor, strokeWidth } = props;
-    return <svg xmlns='http://www.w3.org/2000/svg' width={width} height={height} viewBox="0 0 24 24" fill={fill} 
-    stroke={strokeColor} strokeWidth={strokeWidth}  strokeLinecap='round' strokeLinejoin='round'>
-    <path fill={fill} stroke='none' d='M0 0h24v24H0z' />
-    <rect x='4' y='4' width='16' height='16' rx='2' />
-    <line x1='9' y1='12' x2='15' y2='12' />
-    <line x1='12' y1='9' x2='12' y2='15' />
-  </svg>
+    return <svg xmlns='http://www.w3.org/2000/svg' width={width} height={height} viewBox="0 0 24 24" fill={fill}
+        stroke={strokeColor} strokeWidth={strokeWidth} strokeLinecap='round' strokeLinejoin='round'>
+        <path fill={fill} stroke='none' d='M0 0h24v24H0z' />
+        <rect x='4' y='4' width='16' height='16' rx='2' />
+        <line x1='9' y1='12' x2='15' y2='12' />
+        <line x1='12' y1='9' x2='12' y2='15' />
+    </svg>
 };
 
 export default AddIcon;
--- src/component/addNewTask/index.js
+++ src/component/addNewTask/index.js
@@ -1,4 +1,4 @@
-import { useCallback, useState} from 'react';
+import { useCallback, useState } from 'react';
 import React from 'react';
 import styled from 'styled-components';
 import Input from '../input';
@@ -6,38 +6,38 @@ import IconButton from '../IconButton';
 import AddIcon from '../../assets/addIcon';
 
 const Container = styled.div`
-  margin: 1.5rem auto;
-  padding: 1rem;
-  width: 70vw;
-  display: flex;
-  border: 2px solid #757070;
-  border-radius: 0.5rem;
-  align-items: center;
+    margin: 1.5rem auto;
+    padding: 1rem;
+    width: 70vw;
+    display: flex;
+    border: 2px solid #757070;
+    border-radius: 0.5rem;
+    align-items: center;
 `;
 
 const AddNewTask = React.memo(props => {
-  const [taskName, setTaskName] = useState('');
-  const { handleAddNewTask } = props;
+    const [taskName, setTaskName] = useState('');
+    const { handleAddNewTask } = props;
 
- 
-  const handleInputTextChange = useCallback((e) => setTaskName(e.target.value), []);
 
-  const handleAddButton = useCallback(() => {
-    const currTask = { name: taskName, completed: false };
-    setTaskName('');
-    handleAddNewTask(currTask);
-  }, [taskName, handleAddNewTask]);
+    const handleInputTextChange = useCallback((e) => setTaskName(e.target.value), []);
 
-  return <Container>
-    <Input placeholderValue={'Add New Task'} rows={'1'} inputText={taskName}
-      handleChange={handleInputTextChange} style={{ flexGrow: '1' }} />
-    <IconButton onClick={handleAddButton}
-      style={{
-        padding: '0.5rem',
-      }} >
-      <AddIcon />
-    </IconButton>
-  </Container>;
+    const handleAddButton = useCallback(() => {
+        const currTask = { name: taskName, completed: false };
+        setTaskName('');
+        handleAddNewTask(currTask);
+    }, [taskName, handleAddNewTask]);
+
+    return <Container>
+        <Input placeholderValue={'Add New Task'} rows={'1'} inputText={taskName}
+            handleChange={handleInputTextChange} style={{ flexGrow: '1' }} />
+        <IconButton onClick={handleAddButton}
+            style={{
+                padding: '0.5rem',
+            }} >
+            <AddIcon />
+        </IconButton>
+    </Container>;
 });
 
 export default AddNewTask;
