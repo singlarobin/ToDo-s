730cea986c91cd0c1c24ef94bd99a9b62e0623da
singlarobin <singlarobin99@gmail.com>
2021-09-17 00:13:40 +0530

    theme colors added

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { useEffect, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 const AppContainer = styled.div`
-	background: ${({ theme }) => theme.pageBg};
+	background: ${({ theme }) => theme.backgroundGradient};
   	height:inherit;
   	width:inherit;
 `;
--- src/resource/theme.js
+++ src/resource/theme.js
@@ -6,6 +6,16 @@ const lightTheme = {
     primaryTheme: '#DD7F7A',
     secondaryTheme: '#EBE8D6',
     buttonColor: '#000',
+
+    accentColor1: '#B3C8FF',
+    accentColor2: '#BAC9FF',
+    accentColor3: '#EED3FF',
+    backgroundGradient: 'linear-gradient(to right, #B3C8FF, #BAC9FF, #EED3FF)',
+
+    textHeading: '#102F81',
+    textNormal: '#20795E',
+
+    iconColor: '#102F81'
 }
 
 const darkTheme = {
@@ -16,6 +26,16 @@ const darkTheme = {
     primaryTheme: '#DD7F7A',
     secondaryTheme: '#EBE8D6',
     buttonColor: '#fff',
+
+    accentColor1: '#102F81',
+    accentColor3: '#2E87A3',
+
+    backgroundGradient: 'linear-gradient(to right, #102F81, #2E87A3)',
+
+    textHeading: '#E0CEEC',
+    textNormal: '#812121',
+
+    iconColor: '#1B6666'
 }
 
 export {
